37f9d4dea6b35ff49d8155b8246f4bd74dc3086d
Igor Krivenko <igor.s.krivenko@gmail.com>
2017-05-23 00:25:21 -0400

    [doc] Emphasize Lewin's and Hugo's contributions to G^2 measures

--- c++/measures/g2.hpp
+++ c++/measures/g2.hpp
@@ -2,7 +2,7 @@
  *
  * TRIQS: a Toolbox for Research in Interacting Quantum Systems
  *
- * Copyright (C) 2017, I. Krivenko, H. U.R. Strand,M. Ferrero and O. Parcollet
+ * Copyright (C) 2017, I. Krivenko, L. Boehnke, H. U.R. Strand,M. Ferrero and O. Parcollet
  *
  * TRIQS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
